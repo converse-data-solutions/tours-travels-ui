df267f8a95cdacf605cf376fc606efe2eee11317
Nandhinivelu <nandhini@conversedatasolutions.com>
2024-01-27 17:22:27 +0530

    focus outline added

--- app/components/CommonComponents/PackageSearchBar.tsx
+++ app/components/CommonComponents/PackageSearchBar.tsx
@@ -15,7 +15,7 @@ const PackageSearchBar: React.FC<SearchBarProps> = ({
     <input
       type="text"
       placeholder={placeholder}
-      className="border-[1px] w-full px-4 py-3 mb-2 lg:mb-[0px] mt-[2px] border-gray-200 rounded-lg outline-none md:w-1/3  lg:w-[100%] lg:h-[48px] "
+      className="border-[1px] w-full px-4 py-3 mb-2 lg:mb-[0px] mt-[2px] border-gray-200 rounded-lg outline-none md:w-1/3  lg:w-[100%] lg:h-[48px] focus:outline-none focus:border-gray-300"
       value={searchQuery}
       onChange={(e) => setSearchQuery(e.target.value)}
     />
